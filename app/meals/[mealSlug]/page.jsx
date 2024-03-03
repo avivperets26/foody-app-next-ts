4de4fa46310b7a27ee20970633b94298fa4581c3
Aviv Perets <avivperets26@gmail.com>
2024-03-03 13:01:32 +0000

    add dynamic metadata

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -3,6 +3,15 @@ import styledClasses from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
+export async function generateMetadata(params) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+  return { title: meal.title, description: meal.summery };
+}
+
 const MealDetailsPage = ({ params }) => {
   const meal = getMeal(params.mealSlug);
   if (!meal) {
